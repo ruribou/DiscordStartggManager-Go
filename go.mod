5bfd59b04ea9901ca44b0b12f1e822ed2f4af56e
ruribou <ruribou@hotmail.com>
2025-09-08 23:24:52 +0900

    add: 一旦 Hello World しつつ go mod init もしちゃう

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ruribou/DiscordStartggManager-Go
+
+go 1.25.0
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "github.com/ruribou/DiscordStartggManager-Go/src"
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
